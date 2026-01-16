a2286f90a678112de1cd5c2adf7e8a900368f39c
Noor Anwar <nour422533125@gmail.com>
2026-01-17 02:03:37 +0400

    add task creation route

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
+# Temporary in-memory storage (later we’ll use SQLite)
+tasks = []
+
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return render_template("index.html", tasks=tasks)
+
+@app.route("/add", methods=["POST"])
+def add_task():
+    title = request.form.get("title")
+    if title:
+        tasks.append(title)
+    return redirect(url_for("home"))
 
 if __name__ == "__main__":
     app.run(debug=True)
